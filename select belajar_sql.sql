f4c14d25f7bda31b5d1f0bd644f9b673aa3b6721
purnomo yusgiantoro <pyxvin124@gmail.com>
2025-02-24 06:09:39 +0700

    Add files via upload

---
+++ select belajar_sql.sql
@@ -0,0 +1,13 @@
+select *
+from Parks_and_Recreation.employee_demographics;
+
+select first_name,last_name,birth_date, age,(age + 10)-10
+from Parks_and_Recreation.employee_demographics;
+
+
+select distinct	 first_name,gender
+from Parks_and_Recreation.employee_demographics;
+
+
+
+
