2255f37d32580e021b1652df581ed808cdb4e5c2
purnomo yusgiantoro <pyxvin124@gmail.com>
2025-02-27 19:29:00 +0700

    Add files via upload

---
+++ avgsumcount_belajar_sql.sql
@@ -0,0 +1,22 @@
+select *
+from employee_salary
+;
+
+select avg(salary)
+from employee_salary
+;
+
+SELECT AVG(salary) as avarage
+FROM employee_salary
+WHERE salary > 60000;
+
+SELECT avg(salary) as rata_rata
+FROM employee_salary;
+
+SELECT sum(salary) as total
+FROM employee_salary;
+
+
+
+
+
