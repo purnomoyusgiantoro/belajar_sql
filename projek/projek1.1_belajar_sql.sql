34706bb8942f197d42390efe078626aebf9487dc
purnomo yusgiantoro <pyxvin124@gmail.com>
2025-03-01 21:31:05 +0700

    Add files via upload

---
+++ projek/projek1.1_belajar_sql.sql
@@ -0,0 +1,38 @@
+select *
+from diabetes;
+
+DESC diabetes;
+
+SELECT * FROM diabetes WHERE 
+    SkinThickness IS NULL OR 
+    Pregnancies IS NULL OR 
+    Outcome IS NULL OR 
+    Insulin IS NULL OR 
+    Glucose IS NULL OR 
+    DiabetesPedigreeFunction IS NULL OR 
+    BMI IS NULL OR 
+    BloodPressure IS NULL OR 
+    Age IS NULL;
+
+DELETE FROM diabetes WHERE 
+    SkinThickness IS NULL OR 
+    Pregnancies IS NULL OR 
+    Outcome IS NULL OR 
+    Insulin IS NULL OR 
+    Glucose IS NULL OR 
+    DiabetesPedigreeFunction IS NULL OR 
+    BMI IS NULL OR 
+    BloodPressure IS NULL OR 
+    Age IS NULL;
+    
+select insulin,BMI,DiabetesPedigreeFunction,Age
+from diabetes
+order by Age asc;
+
+create table 
+
+    
+    
+    
+
+
