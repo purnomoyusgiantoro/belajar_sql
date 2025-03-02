d0710928907d2b668043e74df31acf3d07093762
purnomo yusgiantoro <pyxvin124@gmail.com>
2025-03-02 14:40:59 +0700

    Add files via upload

--- projek3_sosialmedia/projek3_sosialmedia_belajar_sql.sql
+++ projek3_sosialmedia/projek3_sosialmedia_belajar_sql.sql
@@ -1,1 +1,34 @@
-SELECT * FROM time_sm.Time_Wasters;
+SELECT * 
+FROM Time_Wasters;
+
+-- identifikasi umur dengan paltform
+
+select Age, Gender , Platform
+from Time_Wasters;
+
+select Age, Gender , Platform
+from Time_Wasters;
+
+select Age, Gender , Platform
+from Time_Wasters
+order by Platform;
+
+-- rata rata Platform gender  by age
+
+select Platform, avg(Age) as avg_age
+from Time_Wasters
+group by Platform;
+
+select Gender, avg(Age) as avg_gender
+from Time_Wasters
+group by Gender;
+
+select Location, avg(Age) as avg_location
+from Time_Wasters
+group by Location;
+
+-- identifikasi jumlah penggunaan
+
+
+
+
