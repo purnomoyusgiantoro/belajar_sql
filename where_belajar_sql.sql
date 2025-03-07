5972ecca6a351c8d37383150109b79fb0e9839b0
purnomo yusgiantoro <pyxvin124@gmail.com>
2025-03-07 08:47:14 +0700

    Add files via upload

--- where_belajar_sql.sql
+++ where_belajar_sql.sql
@@ -1,57 +1,10 @@
+SELECT * FROM Parks_and_Recreation.employee_demographics;
 
--- where clause
 
-select *
-from employee_salary
-where first_name = 'Leslie' 
-;
-
-select *
-from employee_salary
-where salary >= 50000
-;
-
-select *
+select employee_id
 from employee_demographics
-where gender != 'female'
-;
+where employee_id between 5 and 10
 
-select *
+select employee_id
 from employee_demographics
-where birth_date > '1985-01-01'
-;
-
-
--- and or not
-
-select *
-from employee_demographics
-where birth_date > '1985-01-01'
-or not gender  = 'male'
-;
-
-select *
-from employee_demographics
-where (first_name = 'leslie' and age = 44) or age > 55
-;
-
-
--- like statamen
-
-select *
-from employee_demographics
-where first_name like'__S__%'
-;
-
-
-
-
-
-
-
-
-
-
-
-
-
+where
