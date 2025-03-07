1ae8b3f2220bca53ca0c3f4852dbaafcb6419e54
purnomo yusgiantoro <pyxvin124@gmail.com>
2025-03-07 08:50:57 +0700

    Add files via upload

--- where_belajar_sql.sql
+++ where_belajar_sql.sql
@@ -3,8 +3,26 @@ SELECT * FROM Parks_and_Recreation.employee_demographics;
 
 select employee_id
 from employee_demographics
-where employee_id between 5 and 10
+where employee_id between 5 and 10;
 
-select employee_id
+select *
 from employee_demographics
-where
+where age = 44 ;
+
+select *
+from employee_demographics
+where age != 44 ;
+
+select *
+from employee_demographics
+where age >= 44;
+
+select *
+from employee_demographics
+where age <= 36;
+
+select *
+from employee_demographics
+where age >= 44;
+
+
